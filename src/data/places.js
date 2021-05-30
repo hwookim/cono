d7711c249fcde438fa1062022703ec8ddae19ca7
hwookim <gusdn9754@gmail.com>
2021-05-31 02:22:23 +0900

    chore: 임시 데이터 추가

---
+++ src/data/places.js
@@ -0,0 +1,31 @@
+const PLACES = [
+  {
+    name: "24시동국대코인노래연습장",
+    address: "서울 중구 서애로1길 10",
+    latitude: 37.56091632460447,
+    longitude: 126.99844451410752,
+    price: "3곡에 천원",
+    photo:
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210322_282%2F1616397113495viWn7_JPEG%2F3rlflNXEAm3dMyUTZQxsGPf0.jpg",
+  },
+  {
+    name: "제이코인노래연습장",
+    address: "서울 중구 필동로 3 삼지빌딩 지하",
+    latitude: 37.56135575162827,
+    longitude: 126.99620195961862,
+    price: "3곡에 천원",
+    photo:
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190917_171%2F1568698196428qh6Jb_JPEG%2FPHeG2DFIUs56vXllopivLi8u.jpg",
+  },
+  {
+    name: "리코스타 코인노래방",
+    address: "서울 중구 퇴계로 222",
+    latitude: 37.56152409392275,
+    longitude: 126.9963816693589,
+    price: "3곡에 천원",
+    photo:
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20191020_93%2F15715039352878rn9i_JPEG%2FrIEUlE2VIiCpln-KWDUv97-L.jpg",
+  },
+];
+
+export { PLACES };
