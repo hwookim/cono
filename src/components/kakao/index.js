1ee54a0209915080c2f66929ba31aceac048bdad
hwookim <gusdn9754@gmail.com>
2021-05-31 21:38:35 +0900

    refactor: kakao-api 이용 부분 모듈화

---
+++ src/components/kakao/index.js
@@ -0,0 +1,47 @@
+import { MARKER_IMG } from "../../constants/constants";
+
+const { kakao } = window;
+
+const $kakao = (() => {
+  let map;
+
+  const initMap = (targetID) => {
+    const container = document.getElementById(targetID);
+    const options = {
+      center: new kakao.maps.LatLng(37.561371221033546, 126.99740576852703),
+    };
+
+    map = new kakao.maps.Map(container, options);
+  };
+
+  const drawMarker = (location) => {
+    const width = 36;
+    const height = 36;
+    const imageSize = new kakao.maps.Size(width, height);
+    const imageOption = { offset: new kakao.maps.Point(width / 2, height) };
+
+    const markerImage = new kakao.maps.MarkerImage(
+      MARKER_IMG,
+      imageSize,
+      imageOption
+    );
+    const markerPosition = new kakao.maps.LatLng(
+      location.latitude,
+      location.longitude
+    );
+
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage,
+    });
+
+    marker.setMap(map);
+  };
+
+  return {
+    initMap,
+    drawMarker,
+  };
+})();
+
+export default $kakao;
--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import $kakao from "../kakao";
 
 import NavigationBar from "../NavigationBar";
-
-import { PLACES } from "../../data/places";
-import { MARKER_IMG } from "../../constants/constants";
 import SearchBar from "../SearchBar";
 import NearPlaceFloatingInfo from "./NearPlaceFloatingInfo";
 import PlaceList from "./PlaceList";
 
-const { kakao } = window;
+import { PLACES } from "../../data/places";
 
 const Container = styled.div`
   display: flex;
@@ -24,42 +22,10 @@ const KakaoMap = styled.div`
 `;
 
 export default function MapPage() {
-  let map;
-
   useEffect(() => {
-    const container = document.getElementById("kakao-map");
-    const options = {
-      center: new kakao.maps.LatLng(37.561371221033546, 126.99740576852703),
-    };
-
-    map = new kakao.maps.Map(container, options);
-
-    PLACES.forEach(drawMarker);
+    $kakao.initMap("kakao-map");
+    PLACES.map($kakao.drawMarker);
   }, []);
-
-  const drawMarker = (place) => {
-    const width = 36;
-    const height = 36;
-    const imageSize = new kakao.maps.Size(width, height);
-    const imageOption = { offset: new kakao.maps.Point(width / 2, height) };
-
-    const markerImage = new kakao.maps.MarkerImage(
-      MARKER_IMG,
-      imageSize,
-      imageOption
-    );
-    const markerPosition = new kakao.maps.LatLng(
-      place.latitude,
-      place.longitude
-    );
-
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-      image: markerImage,
-    });
-
-    marker.setMap(map);
-  };
 
   return (
     <Container>
