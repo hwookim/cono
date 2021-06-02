093cd122a7a017f3d0ef805e0fc86c690dcfa799
hwookim <gusdn9754@gmail.com>
2021-06-02 13:38:12 +0900

    refactor: 전체 높이 설정 코드 내 중복 제거

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+(() => {
+  const setFullHeightProperty = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
 
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+  setFullHeightProperty();
+  window.addEventListener("resize", setFullHeightProperty);
+})();
 
 ReactDOM.render(
   <React.StrictMode>
