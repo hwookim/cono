1858dfc9bce3e926371779e8a45b6d6b9c75d130
hwwokim <gusdn9754@gmail.com>
2021-06-08 15:45:08 +0900

    style: 네비게이션 바 크기 및 padding 수정

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -10,10 +10,10 @@ const Container = styled.nav`
   z-index: 999;
   position: fixed;
   bottom: 0;
-  width: 80vw;
-  max-width: 500px;
+  width: 90vw;
+  max-width: calc(500px - 10vw);
   height: 48px;
-  padding: 2px 10%;
+  padding: 2px 5vw;
   display: flex;
   justify-content: space-between;
   box-shadow: 0 -3px 5px 2px ${COLORS.MAIN_GRAY};
